6e5ab3fac4cb157c06e9d1fe972503c431bfd8af
FrancimarAlexandre <francimaralexandredt@gmail.com>
2025-01-04 07:35:40 -0300

    iniciando rotas

---
+++ src/main/routes/routes.py
@@ -0,0 +1,30 @@
+from flask import jsonify,Blueprint,request
+
+lanche_routes_bp = Blueprint("lanche_routes",__name__)
+
+# Gestão de produtos
+
+def get_produtos():
+    pass
+def get_produto_id(id):
+    pass
+def post_produto():
+    pass
+def put_produto(id):
+    pass
+def delete_produto(id):
+    pass
+
+# Gestão de Pedidos
+
+def get_pedido():
+    pass
+def get_pedido_id(id):
+    pass
+def post_pedido():
+    pass
+def put_pedido(id):
+    pass
+def delete_pedido(id):
+    pass
+
